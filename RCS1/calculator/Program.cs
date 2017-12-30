e42d20baf2b255c1eba727db4537e9aff24e988f
ieva77 <stegmane.ieva@gmail.com>
2017-12-31 00:20:41 +0200

    pievienojasm

---
+++ RCS1/calculator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // izveidot kalkulēšanas objektu
+            calculations calc;
+            calc = new calculations();
+
+            // paprasīt lietotājam vērtīu
+            int firstNumber = calc.AskUserForNumber();
+
+            // paprasīt otru vērtību
+            int secondNumber = calc.AskUserForNumber();
+
+            // saskaita
+            int result = firstNumber + secondNumber;
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RCS1/calculator/calculations.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+    class calculations
+    {
+        public int AskUserForNumber()
+        {
+            // izvadīt tekstu kas paprasa ciparu
+            Console.WriteLine("please enter number:");
+            // ielasīt ciparu no konsoles
+            // izveido mainīgo, kas glaās tekstu
+            int number;
+            // ieraksta mainīgajā ReadLine funkcijas rezultātu
+            string inputText = Console.ReadLine();
+
+            // pārveido lietotaja tekstu par ciparu
+            bool success  = Int32.TryParse(inputText, out number);
+
+            if (success == false)
+            {
+                Console.WriteLine("sorry, wrong value entered");
+                number = this.AskUserForNumber();
+            }
+
+            return number;
+        }
+    }
+}
