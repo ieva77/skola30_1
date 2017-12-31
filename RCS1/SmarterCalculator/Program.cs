9569d7e360504f5edda60d4fdd96b3b7c468e9ef
ieva77 <stegmane.ieva@gmail.com>
2017-12-31 12:21:39 +0200

    added

---
+++ RCS1/SmarterCalculator/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmarterCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // izveido kalkulatora objektu
+
+            // paprasīt lietotājam ievadīt ievadi
+            Console.WriteLine("please enter darība");
+            string input = Console.ReadLine();
+            // "1 +5 -4" skaits ir 5 pēdējā simbola pozīcija ir 4 
+            // "1+" skaits ir 2 pēdēja pozīcija ir 1
+            int result;
+            int counter = 0;
+            while (counter < input.Length)
+            {
+                char symbol = input[counter];
+                if (symbol == '+')
+                {
+                    Console.WriteLine("plus");
+                }
+                else
+                {
+                    int number;
+                    number = Int32.Parse(symbol.ToString());
+                    Console.WriteLine("number " + number);
+
+                }
+
+                counter = counter + 1;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
