e7b08e8d736e8c1416ec8f089b6c44cb3096e0c2
ieva77 <stegmane.ieva@gmail.com>
2018-01-04 17:45:38 +0200

    kometārs

---
+++ RCS1/CatDtingSite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CatDtingSite.MvcApplication" Language="C#" %>
---
+++ RCS1/CatDtingSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CatDtingSite.Startup))]
+namespace CatDtingSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
